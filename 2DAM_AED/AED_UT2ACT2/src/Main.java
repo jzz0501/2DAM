492415de5e85053f08d1265dde9e4cc2587a79d3
ZongZhen_2DAM <jiangzongzhen51@gmai.com>
2023-10-10 14:04:54 +0200

    commit

--- 2DAM_AED/AED_UT2ACT2/src/Main.java
+++ 2DAM_AED/AED_UT2ACT2/src/Main.java
@@ -1,6 +1,25 @@
+import java.sql.*;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+
+
+        Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres","19871124");
+        PreparedStatement statement = connection.prepareStatement("insert into dep values (?,?,?)");
+        statement.setInt(1,2);
+        statement.setString(2,"name2");
+        statement.setString(3,"localidad2");
+        statement.execute();
+
+        Statement statement2 = connection.createStatement();
+        ResultSet rs = statement2.executeQuery("select*from dep");
+
+        while(rs.next()) {
+            System.out.println(rs.getString("id_dep"));
+            System.out.println(rs.getString("name_dep"));
+            System.out.println(rs.getString("localidad"));
+        }
 
 
     }
