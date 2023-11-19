c22dddcf721a39145298de9c3921b6d830feadcf
zongzhenjiang <jiangzongzhen51@gmail.com>
2023-11-19 22:26:26 +0000

    commit

--- 2DAM_PGV/Filosofo/src/Main.java
+++ 2DAM_PGV/Filosofo/src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         Tenedor between4_5 = new Tenedor("4_5");
         Tenedor between5_1 = new Tenedor("5_1");
 
-        new Filosofo("Filosofo 1", between5_1, between1_2).start();
+        new Filosofo("Filosofo 1", between1_2, between5_1).start();
         new Filosofo("Filosofo 2", between1_2, between2_3).start();
         new Filosofo("Filosofo 3", between2_3, between3_4).start();
         new Filosofo("Filosofo 4", between3_4, between4_5).start();
--- 2DAM_PGV/Filosofo/src/Tenedor.java
+++ 2DAM_PGV/Filosofo/src/Tenedor.java
@@ -1,22 +1,13 @@
 public class Tenedor {
 
     private String name;
-    private boolean isUsing;
 
     public Tenedor(String name) {
         this.name = name;
-        this.isUsing = false;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setUsing(boolean using) {
-        isUsing = using;
-    }
-
-    public boolean isUsing() {
-        return isUsing;
-    }
 }
